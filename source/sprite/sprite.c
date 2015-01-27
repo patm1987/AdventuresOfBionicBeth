483c094373ee918a847e5b35528785878b5f7683
patm1987 <pux1987@gmail.com>
2015-01-26 20:56:18 -0700

    An object is rendering, my background is not

--- source/sprite/sprite.c
+++ source/sprite/sprite.c
@@ -1,12 +1,15 @@
 
 #include "sprite.h"
 
+#include <gba_dma.h>
 #include <gba_video.h>
+#include <string.h> // memcpy
 
 u16 SPGetMode() {
-	return OBJ_1D_MAP;
+	return OBJ_1D_MAP | OBJ_ON;
 }
 
 void SPLoadPalettes(palette_t* paPalettes, u32 count) {
-	dmaCopy(paPalettes, SPRITE_PALETTE, sizeof(palette_t) * count);
+	// dmaCopy(paPalettes, SPRITE_PALETTE, sizeof(palette_t) * count);
+	memcpy(SPRITE_PALETTE, paPalettes, sizeof(palette_t) * count);
 }
